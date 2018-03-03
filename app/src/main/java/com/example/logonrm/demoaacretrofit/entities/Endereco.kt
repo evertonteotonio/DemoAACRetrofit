f76f42c2a2ea8f80213b093f92d5b2a5df49045c
logonrm <logonrm@fiap.com.br>
2018-03-03 14:21:45 -0300

    Começando o DEV

--- app/src/main/java/com/example/logonrm/demoaacretrofit/entities/Endereco.kt
+++ app/src/main/java/com/example/logonrm/demoaacretrofit/entities/Endereco.kt
@@ -3,7 +3,9 @@ package com.example.logonrm.demoaacretrofit.entities
 /**
  * Created by logonrm on 03/03/2018.
  */
-data class Endereco (val cep: String,
+
+/*
+* data class Endereco (val cep: String,
                      val logradouro: String,
                      val complemento: String,
                      val bairro: String,
@@ -12,3 +14,12 @@ data class Endereco (val cep: String,
                      val unidade: String,
                      val ibge: String,
                      val gia: String)
+*
+* */
+
+data class Endereco (val cep: String,
+                     val logradouro: String,
+                     val complemento: String,
+                     val bairro: String,
+                     val localidade: String,
+                     val uf: String)
--- app/src/main/java/com/example/logonrm/demoaacretrofit/repositories/EnderecoRepository.kt
+++ app/src/main/java/com/example/logonrm/demoaacretrofit/repositories/EnderecoRepository.kt
@@ -1,7 +1,13 @@
 package com.example.logonrm.demoaacretrofit.repositories
 
+import android.arch.lifecycle.LiveData
+import com.example.logonrm.demoaacretrofit.entities.EnderecoResponse
+
 /**
  * Created by logonrm on 03/03/2018.
  */
-class EnderecoRepository {
+interface  EnderecoRepository {
+
+    fun BuscarEndereco(cep: String): LiveData<EnderecoResponse>
+
 }
